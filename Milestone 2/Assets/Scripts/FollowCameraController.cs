e110cda3c38fdbeb5568c24c68281a9367af97d7
Idan Mintz <imiintz@gmail.com>
2016-06-21 23:02:17 -0400

    Implemented basics (ragdoll, variable collision box)

---
+++ Milestone 2/Assets/Scripts/FollowCameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCameraController : MonoBehaviour {
+    public Transform target;
+    public float smoothing = 5f;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+	}
+}
---
+++ Milestone 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float SpeedIncrement = 1f;
+    public float HorizontalSpeed = 1f;
+
+    private float forwardSpeed = 0f;
+    private Vector3 speed;
+
+    private Animator animator;
+    private CharacterController ccontroller;
+    private Rigidbody rigidBody;
+    private bool bRagdoll = false;
+
+	// Use this for initialization
+	void Start () {
+        //animator = GetComponentInChildren<Animator>();
+        animator = GetComponent<Animator>();
+        ccontroller = GetComponent<CharacterController>();
+        rigidBody = GetComponent<Rigidbody>();
+        disableRagdoll();
+       
+        speed = new Vector3();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            forwardSpeed += SpeedIncrement * Time.deltaTime;
+        }
+       
+        speed.x = Input.GetAxis("Horizontal") * HorizontalSpeed;
+        //print("Speedx: " + speed.x);
+
+
+        // Update Location
+        //rigidBody.MovePosition(transform.position + speed * Time.deltaTime);
+
+        // Update Animation
+        animator.SetFloat("WalkRun", forwardSpeed/5f);
+
+        //Ragdoll toggle
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (bRagdoll)
+            {
+                disableRagdoll();
+                forwardSpeed = 0;
+                bRagdoll = false;
+            } else
+            {
+                enableRagdoll();
+                bRagdoll = true;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            animator.SetTrigger("Slide"); ;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Jump"); ;
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            animator.SetTrigger("Vault"); ;
+        }
+    }
+
+    void OnCollisionEnter (Collision collision)
+    {
+        print("Collided");
+        if (collision.gameObject.tag == "ObstacleMovable")
+        {
+            print("Hit Movable");
+            // Reduce Speed based on the type of object
+            forwardSpeed -= collision.gameObject.GetComponent<Rigidbody>().mass;
+            
+            // If speed becomes lower than 0, set to 0;
+            if (forwardSpeed < 0)
+            {
+                forwardSpeed = 0;
+            }
+        }
+        if (collision.gameObject.tag == "ObstacleImmovable")
+        {
+            print("Collided with speed: " + forwardSpeed);
+            if (forwardSpeed > 1)
+            {
+                enableRagdoll();
+            }
+            forwardSpeed = 0;
+        }
+    }
+
+    private void enableRagdoll()
+    {
+        animator.enabled = false;
+        foreach (Rigidbody rb in GetComponentsInChildren<Rigidbody>())
+        {
+            rb.isKinematic = false;
+        }
+    }
+
+    private void disableRagdoll()
+    {
+        foreach (Rigidbody rb in GetComponentsInChildren<Rigidbody>())
+        {
+            rb.isKinematic = true;
+        }
+        animator.Rebind();
+        animator.enabled = true;
+        animator.SetTrigger("GetUp");
+    }
+
+}
