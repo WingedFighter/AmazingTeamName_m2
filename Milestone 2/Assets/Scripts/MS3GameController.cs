c4655a1aedbdca8b88dc4ecdfd35f9a57aecbe4e
Idan Mintz <imiintz@gmail.com>
2016-07-15 15:52:33 -0400

    Implemented game controller to dynamically swap dropship types

---
+++ Milestone 2/Assets/Scripts/MS3GameController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MS3GameController : MonoBehaviour {
+    public GameObject DropShip;
+    public PlayerController pc;
+
+    public GameObject[] DropShipTypes;
+
+    public Vector3 DropShipSpawnPosition;
+    public Quaternion DropShipSpawnRotation;
+
+	// Use this for initialization
+	void Start () {
+        DropShipSpawnPosition = DropShip.transform.position;
+        DropShipSpawnRotation = DropShip.transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            DropShip.gameObject.SetActive(false);
+            DropShip = (GameObject)Instantiate(DropShipTypes[0], DropShipSpawnPosition, DropShipSpawnRotation);
+            DropShip.GetComponent<DropShipController>().playerController = pc;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            DropShip.gameObject.SetActive(false);
+            DropShip = (GameObject)Instantiate(DropShipTypes[1], DropShipSpawnPosition, DropShipSpawnRotation);
+            DropShip.GetComponent<DropShipController>().playerController = pc;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            DropShip.gameObject.SetActive(false);
+            DropShip = (GameObject)Instantiate(DropShipTypes[2], DropShipSpawnPosition, DropShipSpawnRotation);
+            DropShip.GetComponent<DropShipController>().playerController = pc;
+        }
+    }
+}
