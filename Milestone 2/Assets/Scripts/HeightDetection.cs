f51fd07b97d7c20bc5627c47882910e371ea7aa4
Kevin Curtin <thekevincurtin@gmail.com>
2016-07-20 22:06:11 -0400

    Added building height detection and game controller
    todo: win screen, UI reset

---
+++ Milestone 2/Assets/Scripts/HeightDetection.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeightDetection : MonoBehaviour {
+	public GameObject levelStatus;
+
+	// Use this for initialization
+	void Start () {		
+	}
+	// Update is called once per frame
+	void Update () {			
+	}
+	void OnTriggerExit(Collider other){
+		//end game
+		if (other.tag == "Building") {
+			levelStatus.tag = "LevelStatusOff";
+		}
+	}
+}
---
+++ Milestone 2/Assets/Scripts/NewGameController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class NewGameController : MonoBehaviour {
+	public GameObject levelStatus;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (levelStatus.tag == "LevelStatusOff") {
+			//move to next level
+			levelStatus.tag = "LevelStatusOn";
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+	}
+}
