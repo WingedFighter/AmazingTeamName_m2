7eed13efae691ee5fc327cd98c5cdba89498d470
yack spillsaps <jmillsaps3@gatech.edu>
2016-07-20 22:39:25 -0400

    use sphereCollider to detected player grounded

--- Milestone 2/Assets/Scripts/PlayerControllerAlpha.cs
+++ Milestone 2/Assets/Scripts/PlayerControllerAlpha.cs
@@ -3,15 +3,16 @@ using System.Collections;
 
 public class PlayerControllerAlpha : MonoBehaviour {
 
-
 	public float decelerationRate = 5f;
     public float HorizontalSpeed = 2f; 
 	public float forwardSpeed = 0f;
+
 	private Vector3 speed;
 	public float time;
 	public float velocitySum;
 	public float animVelocitySum;
 	public int ticks;
+	public bool grounded;
 
 	private Animator animator;
 	private CapsuleCollider myCapsuleCollider;
@@ -27,10 +28,18 @@ public class PlayerControllerAlpha : MonoBehaviour {
 	// public so we can see what's happening
 	public Vector3 addForce;
 
-	private static int OBSTACLES = 8;
+	// Layers
+	private static int OBSTACLES_LAYER = 8;
+	private static int GROUND_LAYER = 10;
+	private static int PLAYER_LAYER = 9;
+
+	// the spherecast will ignore the player layer (don't want to hit colliders on feet)
+	private int sphereColliderLayerMask = ~(1 << PLAYER_LAYER);
 
 	// Use this for initialization
 	void Start () {
+
+		Debug.Log(sphereColliderLayerMask);
         animator = GetComponent<Animator>();
 		myRigidBody = GetComponent<Rigidbody>();
 		myCapsuleCollider = GetComponent<CapsuleCollider>();
@@ -44,8 +53,18 @@ public class PlayerControllerAlpha : MonoBehaviour {
 		velocitySum = 0;
 		animVelocitySum = 0;
 	}
-	
+
 	void FixedUpdate () {
+
+		if (Input.GetKeyDown(KeyCode.G)) {
+			if (getGrounded()) {
+				Debug.Log("grounded");
+			} else {
+				Debug.Log("notGrounded");
+			}
+		}
+
+
 		if (Input.GetKeyDown(KeyCode.Tab) && !animator.IsInTransition(0)) {
 			if (bRagdoll) {
 			 	disableRagdoll();
@@ -144,8 +163,8 @@ public class PlayerControllerAlpha : MonoBehaviour {
     }
 
 	void OnCollisionEnter(Collision myCollision) {
-		if (myCollision.collider.gameObject.layer == OBSTACLES) {
-			Debug.Log("col ENTER  vel = " + myRigidBody.velocity.magnitude);
+		if (myCollision.collider.gameObject.layer == OBSTACLES_LAYER) {
+//			Debug.Log("col ENTER  vel = " + myRigidBody.velocity.magnitude);
 			slowDownOnCollision(myCollision);
 		}
 //		Debug.Log("COL" + myCollisoin.impulse);
@@ -155,8 +174,10 @@ public class PlayerControllerAlpha : MonoBehaviour {
 	}
 
 	void OnCollisionExit(Collision myCollision) {
-		Debug.Log("col EXIT   vel = " + myRigidBody.velocity.magnitude);
-		slowDownOnCollision(myCollision);
+		if (myCollision.collider.gameObject.layer == OBSTACLES_LAYER) {
+//			Debug.Log("col EXIT   vel = " + myRigidBody.velocity.magnitude);
+			slowDownOnCollision(myCollision);
+		}
 //		Debug.Log("COL" + myCollisoin.impulse);
 //		float myVel = myRigidBody.velocity.magnitude;
 //		Debug.Log(myVel);
@@ -168,5 +189,32 @@ public class PlayerControllerAlpha : MonoBehaviour {
 		float myVel = myRigidBody.velocity.magnitude;
 //		Debug.Log(myVel);
 		forwardSpeed = myVel/12.6f;
+	}
+
+	private bool getGrounded() {
+		// Define a Ray from the center of the capsule collider straight down.
+		Vector3 origin = myCapsuleCollider.transform.position 
+			+ new Vector3(0, 0.5f * myCapsuleCollider.height, 0);
+		Vector3 direction = Vector3.down;
+		Ray ray = new Ray(origin, direction);
+
+
+		float sphereRadius = myCapsuleCollider.radius;
+		float castDistance = 0.5f * myCapsuleCollider.height;
+
+		// Cast a sphere the radius of the capsule collider
+		// from the center of the capusule collider straight down,
+		// to the point at which the sphere's center is at the player's feet
+		// and detect if it hits something not on the PLAYER_LAYER 
+		RaycastHit hit;
+		if (Physics.SphereCast(ray, sphereRadius, out hit, castDistance, sphereColliderLayerMask)) {
+			Debug.Log(hit.collider.gameObject.name);
+			// only consider the ground layer 
+			return (hit.collider.gameObject.layer == GROUND_LAYER);
+		} else {
+			return false;
+		}
+
+
 	}
 }
