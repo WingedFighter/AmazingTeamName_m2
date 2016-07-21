09472e07fdc8009a499babe7600186a25049012b
Kevin Curtin <thekevincurtin@gmail.com>
2016-07-20 23:26:16 -0400

    added button & win text

--- Milestone 2/Assets/Scripts/NewGameController.cs
+++ Milestone 2/Assets/Scripts/NewGameController.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class NewGameController : MonoBehaviour {
 	public GameObject levelStatus;
+	public GameObject UIText;
+	public Button button;
+	private Text myText;
 
 	// Use this for initialization
 	void Start () {
-	
+		button.transform.localScale = new Vector3 (0, 0, 0);
+		myText = UIText.GetComponent<Text> ();
+		button.onClick.AddListener(delegate {OnButtonClick();});
 	}
 	
 	// Update is called once per frame
@@ -15,7 +21,17 @@ public class NewGameController : MonoBehaviour {
 		if (levelStatus.tag == "LevelStatusOff") {
 			//move to next level
 			levelStatus.tag = "LevelStatusOn";
-			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+			Win ();
 		}
+	}
+
+	void Win(){
+		//show text & button
+		myText.text = "YOU WIN!";
+		button.transform.localScale = new Vector3 (1, 1, 1);
+	}
+	void OnButtonClick(){
+		//reload scene
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
 	}
 }
