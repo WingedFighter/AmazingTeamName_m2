c7469fd44adea3b8d17ee47c9560250d59f5207a
Idan Mintz <imiintz@gmail.com>
2016-07-25 11:45:04 -0400

    Added Main Menu

---
+++ Milestone 2/Assets/Scripts/MainMenuBuilding.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuBuilding : MonoBehaviour {
+    public bool bCollapsing = false;
+    public bool bDoneCollapsing = false;
+
+    private float baseX;
+    private float baseZ;
+
+	// Use this for initialization
+	void Start () {
+        baseX = transform.position.x;
+        baseZ = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (bCollapsing &! bDoneCollapsing)
+        {
+            Collapse();
+        }
+	}
+
+    public void Collapse()
+    {
+        gameObject.transform.position = new Vector3(baseX + Random.Range(-0.25f, 0.25f), transform.position.y - 4 * Time.deltaTime, baseZ + Random.Range(-0.25f, 0.25f));
+        if(transform.position.y + transform.localScale.y/2f < 0f)
+        {
+            bDoneCollapsing = true;
+        }
+    }
+}
---
+++ Milestone 2/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+    // Buttons
+    public Button PlayButton;
+    public Button LevelSelectButton;
+    public Button CreditsButton;
+
+    private EventSystem eventSystem;
+
+    // Use this for initialization
+    void Start () {
+        eventSystem = EventSystem.current;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level0");
+    }
+
+    public void LevelSelect()
+    {
+        print("Going to level select");
+    }
+
+    public void Credits()
+    {
+
+    }
+}
